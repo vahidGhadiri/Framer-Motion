66a05cc7e6dad2968e5788ae7941196244be5369
vahidGhadiri <whydrf@gmail.com>
2021-08-24 21:37:20 +0430

    [Feature] - Adding more animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {motion} from "framer-motion";
+import {motion} from 'framer-motion';
+
+const buttonVariants = {
+    hover: {
+        scale: 1.1,
+        textShadow: "0px 0px 8px rgb(255,255,255)",
+        boxShadow: "0px 0px 8px rgb(255,255,255)",
+        transition: {
+            duration: 0.3,
+            yoyo: 5
+        }
+    }
+}
+
+const containerVariants = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+        transition: {delay: 1.5, duration: 1.5}
+    },
+    exit: {
+        x: "-100vh",
+        transition: {ease: 'easeInOut'}
+    }
+};
 
 const Home = () => {
     return (
         <motion.div className="home container"
-                    initial={{opacity: 0}}
-                    animate={{opacity: 1}}
-                    transition={{delay: ".3", duration: "1.5"}}
+                    variants={containerVariants}
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
         >
-            <h2>
-                Welcome to Pizza Joint
-            </h2>
+            <h2>Welcome to Pizza Joint</h2>
             <Link to="/base">
                 <motion.button
-                    whileHover={{
-                        scale: 1.05,
-                        textShadow: "0 0 8px rgba(255, 255, 255)",
-                        boxShadow: "0 0 8px rgb(255, 255,255)"
-                    }}
+                    variants={buttonVariants}
+                    whileHover="hover"
                 >
                     Create Your Pizza
                 </motion.button>
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,43 +1,51 @@
 import React from 'react';
-import {motion} from "framer-motion"
-
+import { motion } from 'framer-motion';
 
 const containerVariants = {
-    initial: {
-        opacity: 0,
-        x: 100
-    },
-    animate: {
-        opacity: 1,
-        x: 0,
-        transition: {
-            type: "spring",
-            mass: "0.4",
-            damping: 8,
-            when: "beforeChildren",
-            staggerChildren: ".5"
-        }
-    },
-}
+  hidden: { 
+    opacity: 0, 
+    x: '100vw',
+    transition: {
+      staggerChildren: 0.5,
+    } 
+  },
+  visible: { 
+    opacity: 1, 
+    x: 0,
+    transition: { 
+      type: 'spring',
+      mass: 0.4,
+      damping: 8,
+      staggerChildren: 0.4,
+      when: "beforeChildren",
+    }
+  },
+};
 
 const childVariants = {
-    initial: {opacity: 0},
-    animate: {opacity: 1}
-
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  }
 }
 
-const Order = ({pizza}) => {
-    return (
-        <motion.div className="container order" variants={containerVariants} initial="initial" animate="animate">
-            <h2>Thank you for your order :)</h2>
-            <motion.p variants={childVariants}>You ordered a {pizza.base} pizza
-                with:
-            </motion.p>
-            <motion.div variants={childVariants}>
-                {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-            </motion.div>
-        </motion.div>
-    )
+const Order = ({ pizza }) => {
+  return (
+    <motion.div className="container order"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <h2>Thank you for your order :)</h2>
+      <motion.p variants={childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
+      <motion.div variants={childVariants}>
+        {pizza.toppings.map(topping => <div key={topping} >{topping}</div>)}
+      </motion.div>    
+    </motion.div>
+  )
 }
 
 export default Order;
